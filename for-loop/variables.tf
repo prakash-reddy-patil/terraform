aa524650e4ed70b5aac405447fe36104a5a7369a
prakash-reddy-patil <patilprakashreddy@gmail.com>
2025-12-01 21:39:17 +0530

    for-loop comitted

---
+++ for-loop/variables.tf
@@ -0,0 +1,27 @@
+variable "instances" {
+default=[ "mongodb","redis"]
+
+# default ={
+#     mongodb = "t3.micro"
+#     redis = "t3.micro"
+#     mysql = "t3.small"
+# }
+
+#   default = {
+#     mongodb = {
+#       instance_type = "t3.micro"
+#       ami           = "ami-id"
+#     }
+#     redis = "t3.micro"
+#     mysql = "t3.small"
+#   }
+}
+
+variable "zone_id" {
+    default = "Z04186342ATITR80K0PR0"
+}
+
+variable "domain_name" {
+    default = "divyavutakanti.shop"
+
+}
