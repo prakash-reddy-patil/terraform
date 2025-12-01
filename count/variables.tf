e5263d9dfec373e415013b3a8fac200b2b0dd68a
prakash-reddy-patil <patilprakashreddy@gmail.com>
2025-12-01 13:43:44 +0530

    terraform

---
+++ count/variables.tf
@@ -0,0 +1,12 @@
+variable "instances" {
+    default=["mongodb","redis","mysql","rabbitmq","catalogue","user","cart","shipping","payment","frontend"]
+}
+
+variable "zone_id" {
+    default = "Z04186342ATITR80K0PR0"
+}
+
+variable "domain_name" {
+    default = "divyavutakanti.shop"
+
+}
